baf0ae20c45f4729bc021c6a7d6c5918851e37d0
qd <bitsqiu@gmail.com>
2020-08-22 12:32:08 +0800

    [QD]: finish add goods if goods not exists.

--- src/test/java/com/thoughtworks/rslist/api/GoodsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/GoodsControllerTest.java
@@ -78,4 +78,18 @@ class GoodsControllerTest {
         mockMvc.perform(post("/goods").contentType(MediaType.APPLICATION_JSON).content(json))
                 .andExpect(status().isBadRequest());
     }
+
+    @Test
+    void shouldAddGoodsIfGoodsNotExist() throws Exception{
+        String json = new ObjectMapper().writeValueAsString(Goods.builder()
+                .unit("罐")
+                .price("2.5")
+                .goodsName("可乐")
+                .goodsUrl("https://img11.360buyimg.com/n1/jfs/t4705/83/2924377281/70031/aed9bbd3/58f5629dN79b4406c.jpg")
+                .build());
+        mockMvc.perform(post("/goods").contentType(MediaType.APPLICATION_JSON).content(json))
+                .andExpect(status().isOk());
+
+        assertEquals(4, goodsRepository.findAll().size());
+    }
 }
